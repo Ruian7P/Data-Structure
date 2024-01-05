218b0141dd83845eedbd94f8e55fed4e41536e02
Ruian7P <155224367+Ruian7P@users.noreply.github.com>
2024-01-04 22:50:27 -0500

    Update Stack.java

--- Stack/Stack.java
+++ Stack/Stack.java
@@ -1,32 +1,12 @@
 public interface Stack<E> {
 
-    /**
-     * Returns the number of elements in the stack.
-     * @return number of elements in the stack
-     */
-    int size();
+    int size(); // Returns the number of elements in the stack
 
-    /**
-     * Tests whether the stack is empty.
-     * @return true if the stack is empty, false otherwise
-     */
-    boolean isEmpty();
+    boolean isEmpty(); // Tests whether the stack is empty
 
-    /**
-     * Inserts an element at the top of the stack.
-     * @param e   the element to be inserted
-     */
-    void push(E e);
+    void push(E e); // Inserts an element at the top of the stack
 
-    /**
-     * Returns, but does not remove, the element at the top of the stack.
-     * @return top element in the stack (or null if empty)
-     */
-    E top();
+    E top(); // Returns, but does not remove, the element at the top of the stack
 
-    /**
-     * Removes and returns the top element from the stack.
-     * @return element removed (or null if empty)
-     */
-    E pop();
+    E pop(); //Removes and returns the top element from the stack
 }

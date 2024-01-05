c3963ccca7feabc49ed275f07a33eb3b0cf8685d
Ruian7P <155224367+Ruian7P@users.noreply.github.com>
2024-01-05 00:57:52 -0500

    Update Tree.java

--- Tree/Tree.java
+++ Tree/Tree.java
@@ -17,19 +17,18 @@ class Tree
     {
         t.right =  new Node(o);
     }
-  
-  /** left code are added for expressionTree, but you should know TreeTravelsal method  **/
-  
+    
     public void build()
     {
-        root =  new Node(new Character('+') );
-        leftChild(root, new Character('*') );
-        rightChild(root, new Character('5') );
-        leftChild(root.left, new Character('3'));
-        rightChild(root.left, new Character('4'));
+        root =  new Node(new Character('a') );
+        leftChild(root, new Character('b') );
+        rightChild(root, new Character('c') );
+        leftChild(root.left, new Character('d'));
+        rightChild(root.left, new Character('e'));
+        ......
     }
 
-    public void prefix()  //used as a driver for real prefix method
+    public void prefix()
     {
         prefix(root);
         System.out.println();
@@ -37,16 +36,11 @@ class Tree
 
     private void prefix(Node t)
     {
-        if(t != null)
-        {
-            System.out.print(t);
-            prefix(t.left);
-            prefix(t.right);
-        }
+       
     }
 
 
-    public void infix()  //used as a driver for real infix method
+    public void infix() 
     {
         infix(root);
         System.out.println();
@@ -54,20 +48,9 @@ class Tree
 
     private void infix(Node t)
     {
-        if(t.left != null)  //assume all operators are binary operands
-        {
-            System.out.print("(");
-            infix(t.left);
-        }
-        System.out.print(t );
-        if(t.left != null)  //assume all operators are binary operands
-        {
-            infix(t.right);
-            System.out.print(")");
-        }
     }
 
-    public void postfix()  //used as a driver for real postfix method
+    public void postfix()  
     {
         postfix(root);
         System.out.println();
@@ -75,12 +58,6 @@ class Tree
 
     private void postfix(Node t)
     {
-        if(t != null)
-        {
-            postfix(t.left);
-            postfix(t.right);
-            System.out.print(t);
-        }
     }
 
     public int evaluate()
@@ -90,25 +67,6 @@ class Tree
 
     private int evaluate(Node t)
     {
-        if(t.left == null)	//assume binary operators
-            return (((Character)t.element).charValue()) - '0';
-        else
-        {
-            int left = evaluate (t.left);
-            int right = evaluate (t.right);
-            switch (((Character)t.element).charValue())
-            {
-                case '+':
-                    return left + right;
-                case '-':
-                    return left - right;
-                case '*':
-                    return left * right;
-                case '/':
-                    return left / right;
-            }
-        }
-        return 0;  //we will never get here
     }
 
 }

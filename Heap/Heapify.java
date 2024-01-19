39b3614912fb306bb50a891f27a1feee19b86d2a
Ruian7P <155224367+Ruian7P@users.noreply.github.com>
2024-01-19 18:10:35 -0500

    Create Heapify.java

---
+++ Heap/Heapify.java
@@ -0,0 +1,3 @@
+the foundational method of insert, deleteMin, build_heap
+
+everytime utilizing a method, heapify (picking up the smallest or greatest node as parent node) the triangle (3 node) from bottom right to top left
